7b88df041d24b784eae9265bf85ebf98e75a7c46
hellplatypus <kirill.kreshchenko@gmail.com>
2014-10-25 14:31:16 +0300

    .

--- config.py
+++ config.py
@@ -4,8 +4,5 @@ SECRET_KEY = 'you-will-never-guess'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
